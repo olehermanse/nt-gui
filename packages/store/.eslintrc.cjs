b8d0ea2cf8c9a14987dfec2d931075ff1b837a59
Manuel Zedel <manuel.zedel@northern.tech>
2024-10-22 11:59:37 +0200

    chore(store): aligned w/ Mender store progress & move to TS
    Signed-off-by: Manuel Zedel <manuel.zedel@northern.tech>

--- packages/store/.eslintrc.cjs
+++ packages/store/.eslintrc.cjs
@@ -2,7 +2,7 @@
 module.exports = {
   root: true,
   extends: ['@northern.tech/eslint-config/react.js'],
-  ignorePatterns: ['node_modules/', 'dist/', '**/*.test.js'],
+  ignorePatterns: ['node_modules/', 'dist/', '**/*.test.ts', '**/*.test.tsx', '**/api/types/Settings.ts'],
   globals: {
     mender_environment: true
   }
