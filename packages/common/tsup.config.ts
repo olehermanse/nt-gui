c3816134d9c629ef25ca168da00be0bae9897592
Ihor Aleksandrychiev <ihor.aleksandrychiev@northern.tech>
2024-11-08 11:29:38 +0200

    chore: included auditlogs into common package build
    Signed-off-by: Ihor Aleksandrychiev <ihor.aleksandrychiev@northern.tech>

--- packages/common/tsup.config.ts
+++ packages/common/tsup.config.ts
@@ -3,12 +3,14 @@ import { defineConfig } from 'tsup';
 import { peerDependencies } from './package.json';
 
 export default defineConfig(options => ({
-  entryPoints: ['src/button.tsx'],
+  entryPoints: ['src/button.tsx', 'src/auditlogs/auditlogs.js'],
   format: ['cjs', 'esm'],
   dts: true,
   sourcemap: true,
   target: 'esnext',
   platform: 'browser',
   external: [...Object.keys(peerDependencies)],
+  target: 'esnext',
+  loader: { '.js': 'jsx' },
   ...options
 }));
