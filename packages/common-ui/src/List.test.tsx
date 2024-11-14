b2e5f41c090851de0faa219c99bd144b4dc83434
Manuel Zedel <manuel.zedel@northern.tech>
2024-11-14 14:06:03 +0100

    chore: aligned package local tsconfigs & local file access
    Signed-off-by: Manuel Zedel <manuel.zedel@northern.tech>

--- packages/common-ui/src/List.test.tsx
+++ packages/common-ui/src/List.test.tsx
@@ -11,13 +11,13 @@
 //    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 //    See the License for the specific language governing permissions and
 //    limitations under the License.
-import { CommonList } from '@northern.tech/common-ui/list';
 import { screen } from '@testing-library/react';
 import userEvent from '@testing-library/user-event';
 
 import { defaultState, tenants, undefineds } from '../../../tests/mockData';
 import { render } from '../../../tests/setupTests';
 import { TenantListItem, columnHeaders } from '../components/tenants/TenantList';
+import { CommonList } from './list';
 
 describe('List component', () => {
   it('renders correctly', () => {
